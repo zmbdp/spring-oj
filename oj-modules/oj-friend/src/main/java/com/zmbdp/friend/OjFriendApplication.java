8bdaf0dafd8271893401e7c77dcc8ad65c4fdec9
稚名不带撇 <3566983494@qq.com>
2024-11-04 12:56:23 +0800

    Update OjFriendApplication.java

--- oj-modules/oj-friend/src/main/java/com/zmbdp/friend/OjFriendApplication.java
+++ oj-modules/oj-friend/src/main/java/com/zmbdp/friend/OjFriendApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OjFriendApplication {
     public static void main(String[] args) {
         SpringApplication.run(OjFriendApplication.class, args);
-        System.out.println("nihao");
     }
 }

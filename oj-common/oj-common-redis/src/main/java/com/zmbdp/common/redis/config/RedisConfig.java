050458af545161f42575790269b7be02cf6c7bc0
稚名不带撇 <3566983494@qq.com>
2024-11-09 23:03:08 +0800

    spring-oj项目

--- oj-common/oj-common-redis/src/main/java/com/zmbdp/common/redis/config/RedisConfig.java
+++ oj-common/oj-common-redis/src/main/java/com/zmbdp/common/redis/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.zmbdp.common.redis.config;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;

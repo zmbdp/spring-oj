cae39bbdfe53c4c15ec0eb5aa7633ba9628a6688
稚名不带撇 <3566983494@qq.com>
2024-11-12 03:30:23 +0800

    spring-oj项目

--- oj-modules/oj-system/src/main/java/com/zmbdp/system/test/controller/TestController.java
+++ oj-modules/oj-system/src/main/java/com/zmbdp/system/test/controller/TestController.java
@@ -1,8 +1,10 @@
-package com.zmbdp.system.test;
+package com.zmbdp.system.test.controller;
 
 import com.zmbdp.common.redis.service.RedisService;
 import com.zmbdp.system.domain.SysUser;
+import com.zmbdp.system.test.dto.ValidationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class TestController {
         redisService.setCacheObject("key", sysUser);
         SysUser result = redisService.getCacheObject("key", SysUser.class);
         return result.toString();
+    }
+
+    @RequestMapping("/validation")
+    public String validation(@Validated ValidationDTO validationDTO) {
+        return "参数校验成功";
     }
 }

f181d64f42278c3d543cb6c7600bf196d289192c
稚名不带撇 <3566983494@qq.com>
2024-11-22 01:50:13 +0800

    spring-oj项目

--- oj-modules/oj-system/src/main/java/com/zmbdp/system/domain/question/vo/QuestionVO.java
+++ oj-modules/oj-system/src/main/java/com/zmbdp/system/domain/question/vo/QuestionVO.java
@@ -9,6 +9,6 @@ public class QuestionVO {
     private Long questionId; // 题目 id
     private String title; // 标题
     private Integer difficulty; // 难度
-    private String creatName; // 创建人
+    private String createName; // 创建人
     private LocalDateTime createTime; // 创建时间
 }

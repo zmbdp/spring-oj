05ae1890c410188fe0d251ef07c2ec7ad17405a0
稚名不带撇 <3566983494@qq.com>
2024-12-06 14:56:54 +0800

    spring-oj项目

--- oj-common/oj-common-core/src/main/java/com/zmbdp/common/core/constants/UserConstants.java
+++ oj-common/oj-common-core/src/main/java/com/zmbdp/common/core/constants/UserConstants.java
@@ -1,6 +1,16 @@
 package com.zmbdp.common.core.constants;
 
+import java.util.UUID;
+
 public class UserConstants {
-    public static final String DEFAULT_NICK_NAME = "未命名";
+    public static final String DEFAULT_NICK_NAME = "LaatCode" + generateNickName();
     public static final String DEFAULT_INTRODUCE = "关于你的个性，兴趣或经验，展现自己独一档的强度...";
+    // 生成16位的默认昵称
+    public static String generateNickName() {
+        // 生成一个UUID
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+
+        // 截取前16位作为昵称
+        return uuid.substring(0, 16);
+    }
 }

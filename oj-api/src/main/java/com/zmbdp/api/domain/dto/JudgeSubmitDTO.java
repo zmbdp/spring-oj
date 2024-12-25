50d42ae1a60c72fe9a96ffa5435ac4f9585dc764
稚名不带撇 <3566983494@qq.com>
2024-12-25 20:20:33 +0800

    spring-oj项目

--- oj-api/src/main/java/com/zmbdp/api/domain/dto/JudgeSubmitDTO.java
+++ oj-api/src/main/java/com/zmbdp/api/domain/dto/JudgeSubmitDTO.java
@@ -11,26 +11,26 @@ public class JudgeSubmitDTO {
 
     private Long examId;
 
-    //编程语言类型（0 java 1 C++）
+    // 编程语言类型（0 java 1 C++）
     private Integer programType;
 
     private Long questionId;
 
-    //题目难度
+    // 题目难度
     private Integer difficulty;
 
-    //时间限制 ms
+    // 时间限制 ms
     private Long timeLimit;
 
-    //空间限制 kb
+    // 空间限制 kb
     private Long spaceLimit;
 
-    //拼装完整的用户代码  用户提交的代码 + main函数
+    // 拼装完整的用户代码  用户提交的代码 + main函数
     private String userCode;
 
-    //输入数据
+    // 输入数据
     private List<String> inputList;
 
-    //期望输出数据
+    // 期望输出数据
     private List<String> outputList;
 }

ac9a11a6040af318fa37932e00a2adc703f029b7
稚名不带撇 <118424032+zmbdp@users.noreply.github.com>
2025-01-19 11:30:46 +0800

    Delete deploy/dev/sql/user.sql

--- deploy/dev/sql/user.sql
+++
@@ -1,8 +0,0 @@
-create user 'zmbdp'@'%' identified by '123456';
-create database if not exists `spring_oj_dev`;
-create database if not exists `spring_oj_nacos_local`;
-create database if not exists `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
-
-grant create, drop, select, insert, update, delete, alter on spring_oj_dev.* to 'zmbdp'@'%';
-grant create, drop, select, insert, update, delete, alter on spring_oj_nacos_local.* to 'zmbdp'@'%';
-grant create, drop, select, insert, update, delete, alter on xxl_job.* to 'zmbdp'@'%';
